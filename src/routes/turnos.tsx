67a52caa51e9c60ddbc32e1a9ac6d13bdfd7263e
ramiro.couceiro <ramiro.couceiro@inxpirius.com>
2025-01-18 00:44:13 -0300

    .

--- src/routes/turnos.tsx
+++ src/routes/turnos.tsx
@@ -6,10 +6,7 @@ export default function TurnosPage(){
     const { user } = useUser();
     return(
         <Layout user={user}>
-            <main className="bg-gray-800 text-white w-full mb-20 xl:mb-0">
-                <h1 className="text-2xl font-bold">👋 Buenos días {user?.firstName}!</h1>
-                <Turnos/>
-            </main>
+            <Turnos/>
         </Layout>
     );
 }    
