40256a8b9f1e4d354144491cfbeeca898712278c
Ramiro Couceiro <68604148+ramicouceiro@users.noreply.github.com>
2025-01-17 21:46:17 -0300

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -86,20 +86,20 @@ const Layout: React.FC<LayoutProps> = ({ children, user }) => {
 
       {/* Barra de navegación móvil */}
       <nav className="fixed bottom-0 left-0 right-0 text-white bg-gray-900 flex justify-around text-xl md:hidden">
-        <Link to="/" className={`flex flex-col items-center w-full h-full pb-14 pt-10 ${isActive('/') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
+        <Link to="/" className={`flex flex-col items-center w-full h-full hover:text-green-500 pb-14 pt-10 ${isActive('/') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
           <IoMdHome />
         </Link>
-        <Link to="/planificaciones" className={`flex flex-col items-center w-full h-full pb-14 pt-10 ${isActive('/planificaciones') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
+        <Link to="/planificaciones" className={`flex flex-col items-center w-full h-full hover:text-green-500 pb-14 pt-10 ${isActive('/planificaciones') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
           <FaDumbbell />
         </Link>
-        <Link to="/turnos" className={`flex flex-col items-center w-full h-full pb-14 pt-10 ${isActive('/turnos') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
+        <Link to="/turnos" className={`flex flex-col items-center w-full h-full hover:text-green-500 pb-14 pt-10 ${isActive('/turnos') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
           <FaCalendarAlt />
         </Link>
-        <Link to="/videos" className={`flex flex-col items-center w-full h-full pb-14 pt-10 ${isActive('/videos') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
+        <Link to="/videos" className={`flex flex-col items-center w-full h-full hover:text-green-500 pb-14 pt-10 ${isActive('/videos') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
           <MdSlowMotionVideo />
         </Link>
         {isAdmin && (
-          <Link to="/admin" className={`flex flex-col items-center w-full h-full pb-14 pt-10 ${isActive('/admin') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
+          <Link to="/admin" className={`flex flex-col items-center w-full h-full hover:text-green-500 pb-14 pt-10 ${isActive('/admin') ? 'text-green-500 bg-gray-700' : 'text-white'}`}>
             <FaUserShield />
           </Link>
         )}
